8ce2c2cf34027ea62a6bd1fea49f080b7f30baab
Brett Clapham <bclapham@gmail.com>
2022-09-13 11:25:21 -0700

    first commit

---
+++ wwwroot/scripts/konducter.js
@@ -0,0 +1,11 @@
+function scrollTopLeft(id) {
+    if (document.getElementById(id)) {
+        document.getElementById(id).scrollTop = 0;
+        document.getElementById(id).scrollLeft = 0;
+    }
+}
+function scrollBottom(id) {
+    if (document.getElementById(id)) {
+        setTimeout(function () { document.getElementById(id).scrollTop = document.getElementById(id).scrollHeight }, 200);
+    }    
+}
